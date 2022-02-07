989f3bf32cd04769d4324cde13752295f3d6a65f
lalomx <lalo0892@gmail.com>
2022-02-06 20:23:49 -0600

    add handlebars

--- app.js
+++ app.js
@@ -2,23 +2,35 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const engine = require('./engine');
+const { engine } = require('express-handlebars');
+
+const movies = require('./movies')
+
+// const engine = require('./engine');
 
 const PORT = process.env.PORT || 8080
 
+app.set('view engine', 'hbs');
+app.engine('hbs', engine({
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  extname: 'hbs',
+  defaultLayout: 'index'
+}));
 // decirle a express que vamos a usar un engine ntl (custom)
 // el engine se ejecuta cada que renderizamos un template con res.render
-app.engine("ntl", engine)
+// app.engine("ntl", engine)
 
 // decirle a express que los templates .ntl estaran en la carpeta ./views y que utilice el engine ntl
-app.set('views', './views'); // especifica el directorio de vistas
-app.set('view engine', 'ntl')
+// app.set('views', './views'); // especifica el directorio de vistas
+// app.set('view engine', 'ntl')
 
-// app.use("/static/", express.static(path.join(__dirname, "public")))
+app.use("/static/", express.static(path.join(__dirname, "public")))
 
 // app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")))
 
-app.get("/", (req, res) => res.render("index", { title: "Titulo", message: "message" }))
+// app.get("/", (req, res) => res.render("index", { title: "Titulo", message: "message" }))
+
+app.get("/", (req, res) => res.render("main", { movies, name: 'Lalo' }))
 
 app.listen(
   PORT,
---
+++ movies.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { id: 1, name: 'Joker' },
+  { id: 2, name: 'Good Fellas' },
+  { id: 3, name: 'Matrix' } 
+];
